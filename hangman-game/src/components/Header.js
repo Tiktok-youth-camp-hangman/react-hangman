820bd00abe651ea57aefe7dc776634cbea7b78fd
Serena <siriuswusss@gmail.com>
2022-05-24 00:56:52 +0800

    Install material ui and add a simple header

---
+++ hangman-game/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from "@material-ui/core/"
+
+const Header = () => {
+  return (
+      <>
+          <h1>Hangman</h1>
+          <p>Are you ready to be hung?</p>
+          <Button></Button>
+      </>
+  )
+}
+
+export default Header
