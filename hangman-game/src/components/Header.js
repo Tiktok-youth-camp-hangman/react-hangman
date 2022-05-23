999a97cb11827558534b8b47f8209198acccc464
Serena <siriuswusss@gmail.com>
2022-05-24 01:18:55 +0800

    Improve the header with buttons and styles

--- hangman-game/src/components/Header.js
+++ hangman-game/src/components/Header.js
@@ -1,13 +1,39 @@
 import React from 'react'
-import Button from "@material-ui/core/"
+import Button from "@mui/material/Button"
+import Box from "@mui/material/Box"
 
 const Header = () => {
   return (
-      <>
+      <Box
+          style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center"
+          }}
+      >
           <h1>Hangman</h1>
           <p>Are you ready to be hung?</p>
-          <Button></Button>
-      </>
+          <Box>
+        <Button
+              variant="contained"
+              style={{
+                  textTransform: 'none'
+              }}
+          >
+              Yes! Let's go!
+          </Button>
+          <Button
+              variant="contained"
+              style={{
+                  textTransform: 'none'
+              }}
+          >
+              Uh I guess so...
+          </Button>
+          </Box>
+          
+      </Box>
   )
 }
 
---
+++ hangman-game/src/components/Stand.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Stand = () => {
+  return (
+    <div>Stand</div>
+  )
+}
+
+export default Stand
